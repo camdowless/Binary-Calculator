acb4c41f49ade24e06183846281379c658728720
Mera Alfawares <maalfawares@bsu.edu>
2020-09-10 18:31:59 -0400

    Builds calculator GUI with buttons and display

--- src/GUI2.java
+++ src/GUI2.java
@@ -3,6 +3,18 @@ import java.awt.*;
 
 public class GUI2 {
     private JPanel rootPanel;
+    private JButton zeroButton;
+    private JButton oneButton;
+    private JButton toggleButton;
+    private JButton multiplyButton;
+    private JButton divideButton;
+    private JButton clearButton;
+    private JButton equalButton;
+    private JButton addButton;
+    private JButton subtractButton;
+    private JButton squareButton;
+    private JButton rootButton;
+    private JLabel displayLabel;
 
     public static void main(String[] args){
         JFrame frame = new JFrame("Hello World");
