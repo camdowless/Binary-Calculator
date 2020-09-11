657c1f32f2416743913d85bb021bcbae5c2c7ad7
Nick Hammerstrom <nickh06@hotmail.com>
2020-09-11 16:08:42 -0400

    Updated tests class. Fixed Junit.

---
+++ src/test/Tests.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+public class Tests {
+
+    @Test
+    public void testIfInputTerminal() {
+        //TODO: Implement Test
+    }
+
+    @Test
+    public void testReset(){
+        //TODO: Implement Test
+    }
+
+    @Test
+    public void testCalculate() {
+        //TODO: Implement Test
+    }
+
+    @Test
+    public void testSetN1() {
+        /*
+            i = new int[]{0,0,1,1};
+        */
+    }
+
+    @Test
+    public void testSetN2() {
+        /*
+            i = new int[]{0,1,0,1};
+         */
+    }
+
+    @Test
+    public void testSetOperation() {
+        //TODO: Implement Test
+    }
+}
